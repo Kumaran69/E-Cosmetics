265a9143d34326539d102b903dceb49c56cad6c7
kumaran M <kumarkumaran6253@gmail.com>
2026-07-22 06:11:21 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import './styles/index.css';
 
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
